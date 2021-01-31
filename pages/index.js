f34e27d8c162839e8538fb2d5b9307e9e980abdc
Vinicius de Castro <vinnicastro@hotmail.com>
2021-01-31 14:28:13 -0300

    feat: added eslint and button to play quiz

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import QuizBackground from '../src/components/QuizBackground'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Head from 'next/head'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
-export const QuizContainer = styled.div `
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -16,26 +19,49 @@ export const QuizContainer = styled.div `
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
-  return(
-  <>
-    <Head>
-      <meta property="og:image" content={db.bg} />
-    </Head>
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <meta property="og:image" content={db.bg} />
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Aleatoriedades Biológicas</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsulum...</p>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+              console.log('Submiting form using React');
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Diz ai seu nome!"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                E aí
+                {' '}
+                {name}
+                !? Bora jogar?
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
-        <Widget.Content>
+          <Widget.Content>
             <h1>Quizes da Galera</h1>
             <p>lorem ipsulum...</p>
           </Widget.Content>
@@ -44,6 +70,5 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/vinicastro" />
     </QuizBackground>
-  </>
-  )
+  );
 }
